249e48c1f169c6f8bb958b61a9ce1708db2019d1
antonio <antoniomanzari@gmail.com>
2021-01-21 14:34:05 -0500

    init commit for Pig Latin Capstone

---
+++ GC_Capstone2_PigLatin/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+// Grand Circus Capstone 2: Pig Latin
+// Antonio Manzari
+
+namespace GC_Capstone2_PigLatin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
