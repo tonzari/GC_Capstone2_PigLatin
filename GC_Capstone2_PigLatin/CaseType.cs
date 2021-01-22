f413075710b5b85936a6c05e561b5014ebe25a5a
antonio <antoniomanzari@gmail.com>
2021-01-22 12:49:10 -0500

    added enum for case types

---
+++ GC_Capstone2_PigLatin/CaseType.cs
@@ -0,0 +1,10 @@
+﻿// Grand Circus Capstone 2: Pig Latin
+// Antonio Manzari
+
+namespace GC_Capstone2_PigLatin
+{
+    partial class Program
+    {
+        public enum CaseType { lower = 0 , upper = 1, title = 2 };
+    }
+}
--- GC_Capstone2_PigLatin/Program.cs
+++ GC_Capstone2_PigLatin/Program.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Linq;
+using System.Collections;
+using System.Globalization;
+using System.Threading;
+using System.Collections.Generic;
 
 // Grand Circus Capstone 2: Pig Latin
 // Antonio Manzari
 
 namespace GC_Capstone2_PigLatin
 {
-    class Program
+    partial class Program
     {
         public static string userName = String.Empty;
+        public static CultureInfo cultureInfo = Thread.CurrentThread.CurrentCulture;
+        public static TextInfo textInfo = cultureInfo.TextInfo;
 
         static void Main(string[] args)
         {
@@ -16,7 +22,7 @@ namespace GC_Capstone2_PigLatin
 
             do
             {
-
+                TranslateToPigLatin(GetUserInput());
             } while (CheckUserWantsToContinue());
 
             ExitApp();
@@ -56,9 +62,31 @@ namespace GC_Capstone2_PigLatin
             return userInput;
         }
 
-        public static string TranslateToPigLatin(string sentence)
+        public static void TranslateToPigLatin(string sentence)
         {
-            sentence = sentence.ToLower();
+
+            if (String.IsNullOrWhiteSpace(sentence))
+            {
+                Console.WriteLine($"Oh, {userName}. Trying to break things again, are you? You have to enter *something* to translate! Let's try that again.");
+            }
+            else
+            {
+                String[] words = sentence.Split();
+
+                Dictionary<int, CaseType> savedCaseData = new Dictionary<int, CaseType>();
+
+                for (int i = 0; i < words.Length; i++)
+                {
+                    //check the case, save that info
+
+                    // if contains number, leave it alone
+                    //remove ' from contractions
+
+                }
+
+            }
+
+            // check if just one word, if so tell the user they can do a full sentence.
 
             //if vowel
             //add "way"
@@ -67,10 +95,38 @@ namespace GC_Capstone2_PigLatin
             //move all consonants before first vowel to end
             //add "ay"
 
-            
 
+        }
 
-            return " ";
+        public static bool CheckIsAllUpper(string input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (Char.IsLetter(input[i]) && !Char.IsUpper(input[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool CheckIsAllLower(string input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (Char.IsLetter(input[i]) && !Char.IsLower(input[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        
+
+        public static string ConvertToTitleCase(string input)
+        {
+            return textInfo.ToTitleCase(input);
         }
 
         public static bool CheckUserWantsToContinue()
